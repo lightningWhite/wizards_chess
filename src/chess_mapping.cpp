338425b863151536344da6e53604b0483b74c95b
Daniel Hornberger <hor12020@byui.edu>
2018-03-29 12:38:17 -0600

    fixed mistake

--- src/chess_mapping.cpp
+++ src/chess_mapping.cpp
@@ -9,299 +9,299 @@ std::string mapToGcode(std::string input)
    std::string gcode;
 
    // Map the chess board cell to the gcode coordinate
-   if (input = "a1")
+   if (input == "a1")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "a2")
+   else if (input == "a2")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "a3")
+   else if (input == "a3")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "a4")
+   else if (input == "a4")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "a5")
+   else if (input == "a5")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "a6")
+   else if (input == "a6")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "a7")
+   else if (input == "a7")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "a8")
+   else if (input == "a8")
    {
       gcode = "G90 X0 Y0";
    }
 
-   else if (input = "b1")
+   else if (input == "b1")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "b2")
+   else if (input == "b2")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "b3")
+   else if (input == "b3")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "b4")
+   else if (input == "b4")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "b5")
+   else if (input == "b5")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "b6")
+   else if (input == "b6")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "b7")
+   else if (input == "b7")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "b8")
+   else if (input == "b8")
    {
       gcode = "G90 X0 Y0";
    }
    
-   else if (input = "c1")
+   else if (input == "c1")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "c2")
+   else if (input == "c2")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "c3")
+   else if (input == "c3")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "c4")
+   else if (input == "c4")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "c5")
+   else if (input == "c5")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "c6")
+   else if (input == "c6")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "c7")
+   else if (input == "c7")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "b8")
-   {
-      gcode = "G90 X0 Y0";
-   }
-
-   else if (input = "c1")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "c2")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "c3")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "c4")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "c5")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "c6")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "c7")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "c8")
+   else if (input == "b8")
    {
       gcode = "G90 X0 Y0";
    }
 
-   else if (input = "d1")
+   else if (input == "c1")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "d2")
+   else if (input == "c2")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "d3")
+   else if (input == "c3")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "d4")
+   else if (input == "c4")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "d5")
+   else if (input == "c5")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "d6")
+   else if (input == "c6")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "d7")
+   else if (input == "c7")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "d8")
-   {
-      gcode = "G90 X0 Y0";
-   }
-
-   else if (input = "e1")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "e2")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "e3")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "e4")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "e5")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "e6")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "e7")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "e8")
+   else if (input == "c8")
    {
       gcode = "G90 X0 Y0";
    }
 
-   else if (input = "f1")
+   else if (input == "d1")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "f2")
+   else if (input == "d2")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "f3")
+   else if (input == "d3")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "f4")
+   else if (input == "d4")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "f5")
+   else if (input == "d5")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "f6")
+   else if (input == "d6")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "f7")
+   else if (input == "d7")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "f8")
-   {
-      gcode = "G90 X0 Y0";
-   }
-
-   else if (input = "g1")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "g2")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "g3")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "g4")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "g5")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "g6")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "g7")
-   {
-      gcode = "G90 X0 Y0";
-   }
-   else if (input = "g8")
+   else if (input == "d8")
    {
       gcode = "G90 X0 Y0";
    }
 
-   else if (input = "h1")
+   else if (input == "e1")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "h2")
+   else if (input == "e2")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "h3")
+   else if (input == "e3")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "h4")
+   else if (input == "e4")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "h5")
+   else if (input == "e5")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "h6")
+   else if (input == "e6")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "h7")
+   else if (input == "e7")
    {
       gcode = "G90 X0 Y0";
    }
-   else if (input = "h8")
+   else if (input == "e8")
+   {
+      gcode = "G90 X0 Y0";
+   }
+
+   else if (input == "f1")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "f2")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "f3")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "f4")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "f5")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "f6")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "f7")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "f8")
+   {
+      gcode = "G90 X0 Y0";
+   }
+
+   else if (input == "g1")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "g2")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "g3")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "g4")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "g5")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "g6")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "g7")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "g8")
+   {
+      gcode = "G90 X0 Y0";
+   }
+
+   else if (input == "h1")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "h2")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "h3")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "h4")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "h5")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "h6")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "h7")
+   {
+      gcode = "G90 X0 Y0";
+   }
+   else if (input == "h8")
    {
       gcode = "G90 X0 Y0";
    }
